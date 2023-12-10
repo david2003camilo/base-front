e293bd3db2e7ac6255be75c109954acf407102ed
david-chico <aguja.2003@gmail.com>
2023-12-10 13:24:39 -0500

    feat: add router project

---
+++ src/pages/helloWorldPage/HelloWorldPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { H1Styled, HelloCard } from "./StyleHelloWorld";
+
+const HelloWorldPage = () => {
+  return (
+    <HelloCard>
+      <H1Styled>Hello word</H1Styled>
+    </HelloCard>
+  );
+};
+
+export default HelloWorldPage;
---
+++ src/pages/helloWorldPage/StyleHelloWorld.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HelloCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const H1Styled = styled.h1`
+  color: black;
+`;
